3fea148c21d7c2d03ec69a973f7b7f783230581a
Iankamau1 <theurikamau7@gmail.com>
2023-07-05 18:01:58 +0300

    Completed arithmetic lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let num1 =31
+let num2 =2
+
+const multiply= (num1 * num2)
+
+let random = 0
+random ++
+
+
+let num3 = 5 
+let num4 = 5 
+
+let sum =(num3 + num4)
+
+let mod  = sum % 6
+
+
+
+let max = Math.max(11, 12, 13, 14, 15,18,20);
